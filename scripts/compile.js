1908ac9bda11651e65344e723fff894447324881
Pedro Gaya <pedrogaya@gmail.com>
2023-12-20 12:33:10 -0300

    feat: StudentsContract

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
-const chalk = require("chalk")
 
-const contractName = 'TestContract.sol';
+const contractName = 'StudentsContract.sol';
 const contractPath = path.resolve(__dirname, '../contracts', contractName);
 
 const source = fs.readFileSync(contractPath, 'utf8');
@@ -11,7 +10,7 @@ const source = fs.readFileSync(contractPath, 'utf8');
 const compilerInput = {
   language: 'Solidity',
   sources: {
-    'TestContract.sol': {
+    'StudentsContract.sol': {
       content: source
     }
   },
@@ -31,20 +30,9 @@ const inputString = JSON.stringify(compilerInput);
 
 const output = JSON.parse(solc.compile(inputString));
 
-const contractEVM = output.contracts[contractName]['TestContract']['evm'];
+const contractEVM = output.contracts[contractName]['StudentsContract'];
+const abiJSON = contractEVM['abi']
 
+const bytecode = contractEVM['evm']['bytecode']['object'];
 
-const bytecode = contractEVM['bytecode']['object'];
-
-const gasEstimateTotal = contractEVM['gasEstimates']['creation']['totalCost'];
-
-const gasEstimateExecution = contractEVM['gasEstimates']['creation']['executionCost'];
-
-console.log(chalk.yellow('Total Gas Execution:'));
-console.log(chalk.green(gasEstimateExecution));
-
-console.log(chalk.yellow('Total Gas Estimate:'));
-console.log(chalk.green(gasEstimateTotal));
-
-console.log(chalk.yellow('ByteCode:'));
-console.log(chalk.green(bytecode));
+module.exports = {abiJSON, bytecode};
